ec67d2c4976d1ebd986563bf158a07e08007a07f
hsikelias <plekishsai@gmail.com>
2026-06-30 16:40:08 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+import sys
+import csv
+from rich.console import Console
+from rich.table import Table
+console = Console()
+
+
+# TODO: a progress bar that moves based on the number of films watched vs total number of filmsretty.install()
+# TODO:
+
+username = "vann-neil"
+
+
+print("================================================")
+console.print(f"Welcome to your [dark_orange]Le[/dark_orange][orange1]t[/orange1][chartreuse3]te[/chartreuse3][sea_green2]rb[/sea_green2][deep_sky_blue1]oxd[/deep_sky_blue1] watchlist, {username}!")
+print("================================================")
+print("\n")
+console.print("[italic]This watchlist was last updated on: [bold]6/30/2026[/bold][/italic]")
+print("")
+console.print("")
+
+
